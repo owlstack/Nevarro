6fc75745ac0e2798605de7828e331f781e3a5f13
Sara Morsi <sara.morsi@quadpay.com>
2020-02-09 19:03:21 -0500

    Scaffold for Surfaco Duo App

---
+++ Nevarro/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Nevarro
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            twoPaneView.TallModeConfiguration = TwoPaneViewTallModeConfiguration.TopBottom;
+        }
+
+        void OnNavigateToSample(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            switch (button.Text)
+            {
+                case "Two Page":
+                    Navigation.PushAsync(new TwoPage());
+                    break;
+                case "Companion Pane":
+                    Navigation.PushAsync(new CompanionPane());
+                    break;
+            }
+        }
+    }
+}
